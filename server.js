701c3dfc540a90bb5b62f3deeb1cd25ed5edaa81
NVBMR <30868675+NVBMR@users.noreply.github.com>
2018-02-24 13:38:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('nv',function(req,res){
+    res.send('i am nv');
+    
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
